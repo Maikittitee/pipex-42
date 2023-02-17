5858ebf22624ca89294a6a827094939136f780eb
Krittithee Tuncharoen <ktunchar@z2t8c2.42bangkok.com>
2023-02-17 13:23:01 +0700

    up

--- pipex.c
+++ pipex.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pipex.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: maikittitee <maikittitee@student.42.fr>    +#+  +:+       +#+        */
+/*   By: ktunchar <ktunchar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/09 15:28:05 by maikittitee       #+#    #+#             */
-/*   Updated: 2023/02/17 10:11:42 by maikittitee      ###   ########.fr       */
+/*   Updated: 2023/02/17 13:22:18 by ktunchar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,15 +29,13 @@ void	ft_displayerr(int err, char *msg, int errnum)
 	else if (err == FILE_ERR)
 		msg = ft_strjoin(msg, ": no such file or directory\n");
 	else if (err == CMD_ERR)
-		msg =ft_strjoin(msg, ": command not found\n");
+		msg = ft_strjoin(msg, ": command not found\n");
 	if (msg && (err == FILE_ERR || err == CMD_ERR))
 	{
 		ft_putstr_fd(msg, STDERR_FILENO);
 		free(msg);
 	}
-	exit(errnum);
-	
-
+	exit (errnum);
 }
 
 int	get_path_index(char **env)
@@ -47,7 +45,7 @@ int	get_path_index(char **env)
 	i = 0;
 	while (env[i])
 	{
-		if (ft_strnstr(env[i],"PATH",5))
+		if (ft_strnstr(env[i], "PATH", 5))
 			return (i);
 		i++;
 	}
@@ -59,98 +57,103 @@ char 	**join_bs(char **path)
 	int	i;
 
 	i = 0;
-
 	while (path[i])
 	{
-		path[i] = ft_strjoin(path[i],"/"); //Leak
+		path[i] = ft_strjoin(path[i], "/"); //Leak
 		i++;
 	}
 	return (path);
 }
 
-void	execute(char **path, char **av, char **env,int	indexofcmd)
+void	execute(char **path, char **av, char **env, int indexofcmd)
 {
 	char **cmd;
-	int	i;
+	int		i;
 
 	i = 0;
-
-	cmd = ft_split(av[indexofcmd],' ');
-	if ((access(cmd[0],F_OK)) == -1) // not found
+	cmd = ft_split(av[indexofcmd], ' ');
+	if ((access(cmd[0], F_OK)) == -1) // not found
 	{
 		while (path[i])
 		{
-			path[i] = ft_strjoin(path[i],cmd[0]); //leak
-			if (access(path[i],F_OK) == 0) 
+			path[i] = ft_strjoin(path[i], cmd[0]); //leak
+			if (access(path[i], F_OK) == 0) 
 			{
-				cmd[0] = ft_strjoin(path[i],""); //leak
-				execve(cmd[0],cmd,env);	
+				cmd[0] = ft_strjoin(path[i], ""); //leak
+				execve(cmd[0], cmd, env);	
 			}
 			i++;
 		}
-		ft_displayerr(CMD_ERR, av[indexofcmd],127);
+		ft_displayerr(CMD_ERR, av[indexofcmd], 127);
 	}
-	execve(cmd[0],cmd,env);	
+	execve(cmd[0], cmd, env);	
 }
 
-void	ft_child_process(char **path, char **av, char **env, int fd[2])
+void	ft_child1_process(char **path, char **av, char **env, int fd[2])
 {
 	int	infile_fd;
 
 	infile_fd = open(av[1], O_RDONLY);
 	if (infile_fd < 0)
 		ft_displayerr(FILE_ERR, av[1], errno);
-	dup2(infile_fd,STDIN_FILENO);
-	dup2(fd[1],STDOUT_FILENO);
+	dup2(infile_fd, STDIN_FILENO);
+	dup2(fd[1], STDOUT_FILENO);
 	close(fd[1]);
 	close(fd[0]);
 	close(infile_fd);
-	execute(path,av,env,2);
+	execute(path, av, env, 2);
 }
 
-void	ft_parent_process (char **path, char **av, char **env,int fd[2])
+void	ft_child2_process (char **path, char **av, char **env, int fd[2])
 {
 	int	outfile_fd;
 
 	outfile_fd = open(av[4], O_RDWR | O_CREAT | O_TRUNC, 0777);
 	if (outfile_fd < 0)
 		ft_displayerr(FILE_ERR, av[4], errno);
-	dup2(fd[0],STDIN_FILENO);
-	dup2(outfile_fd,STDOUT_FILENO);
+	dup2(fd[0], STDIN_FILENO);
+	dup2(outfile_fd, STDOUT_FILENO);
 	close(fd[0]);
 	close(fd[1]);
 	close(outfile_fd);
-	execute(path,av,env,3);
-
+	execute(path, av, env, 3);
 }
 
 
-int	main(int ac, char **av,char **env)
+int	main(int ac, char **av, char **env)
 {
-	char **path;
-	int	id;
-	int fd[2];
+	char	**path;
+	int		id;
+	int 	id_in_child;
+	int		fd[2];
 
 	if (ac != 5)
 	{
-		ft_putstr_fd("This program take 4 argument",2);
+		ft_putstr_fd("This program take 4 argument", 2);
 		exit(1);
 	}
-	path = ft_split(env[get_path_index(env)] + 5,':');	
+	path = ft_split(env[get_path_index(env)] + 5, ':');
 	path = join_bs(path);
-	if (pipe(fd) == -1)
-		ft_displayerr(PIPE_ERR, NULL, errno);
 	id = fork();
 	if (id == -1)
 		ft_displayerr(FORK_ERR, NULL, errno);
-
-	if (id == 0) 
+	if (id == 0)
 	{
-		ft_child_process(path,av,env,fd);
+		id_in_child = fork();
+		if (id_in_child == -1)
+			ft_displayerr(FORK_ERR, NULL, errno);
+		if (id_in_child == 0)
+			ft_child1_process(path, av, env, fd);
+		else
+		{
+			wait(NULL);
+			//usleep(1000000);
+			ft_child2_process(path, av, env, fd);	
+		}
 	}
 	wait(NULL);
 	//usleep(1000000);
-	ft_parent_process(path,av,env,fd);
+	ft_putstr_fd("Execute Done c1,c2",2);
+	//ft_parent_process(path, av, env, fd);
 	return (0);
-
 }
